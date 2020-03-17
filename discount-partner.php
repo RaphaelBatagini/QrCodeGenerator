db2b0a727c077fbe2d6cc9b2957f174dd601c59a
Raphael Batagini <raphael.batagini@i9xp.com.br>
2020-03-17 13:06:38 -0300

    Init repository

---
+++ discount-partner.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Discount Partner
+ * Description: Add partner registration for pages where visitors can apply for discounts
+ * Version: 1.0.0
+ * Author: Raphael Batagini
+ * Author URI: https://www.linkedin.com/in/raphael-batagini/
+ * Text Domain: discount-partner
+ * Domain Path: /languages/
+ * License: GPLv3
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+require __DIR__ . '/inc/cpts.php';

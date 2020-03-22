76fdb5b9c12eaa0a61eb937df39e95dfb493e234
Raphael Batagini <raphael.batagini@i9xp.com.br>
2020-03-22 15:37:56 -0300

    Save user registered in database

--- inc/actions.php
+++ inc/actions.php
@@ -15,5 +15,26 @@ add_action(
 );
 
 function discount_partner_register_user(){
-    var_dump($_POST);die;
+    $post_id = wp_insert_post(
+        array(
+            'post_title' => $_POST['email'],
+            'post_type' => 'discount_partner_usr',
+            'post_status' => 'publish',
+        )
+    );
+
+    if (!$post_id) {
+        wp_safe_redirect(get_permalink($_POST['discount_partner']) . '?error=1');
+        exit;
+    }
+
+    add_post_meta($post_id, 'discount_partner', $_POST['discount_partner']);
+    add_post_meta($post_id, 'email', $_POST['email']);
+    add_post_meta($post_id, 'name', $_POST['name']);
+    add_post_meta($post_id, 'gender', $_POST['gender']);
+    add_post_meta($post_id, 'birthdate', $_POST['birthdate']);
+    add_post_meta($post_id, 'phone', $_POST['phone']);
+
+    wp_safe_redirect(get_permalink($_POST['discount_partner']) . '?success=1');
+    exit;
 }

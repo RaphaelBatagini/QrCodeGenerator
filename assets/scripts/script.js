bd1fc70a928cdef6fa60384a111f296746cd2d5d
Raphael Batagini <raphael.batagini@i9xp.com.br>
2020-03-22 11:29:11 -0300

    Add jquery validation and inputmask to register form

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,45 +1,60 @@
 (function($){
-	function formNavigation() {
-		let form = $('form.form-steps');
-		var currentStep = 1;
-		let $nextButton = $('.next');
-        var maxStep = 2;
+	$(document).ready(function(){
+		function formNavigation() {
+			let form = $('form.form-steps');
+			var currentStep = 1;
+			let $nextButton = $('.next');
+			var maxStep = 2;
 
-		// first execution
-        $('.step[data-step="1"]').show();
+			// first execution
+			$('.step[data-step="1"]').show();
 
-		// action on next step click
-		$nextButton.click(function(event) {
-			event.preventDefault();
+			// action on next step click
+			$nextButton.click(function() {
+				if (!validateForm($(this))) {
+					return;
+				}
+			
+				if (currentStep >= maxStep) {
+					return;
+				}
 
-			if (currentStep >= maxStep) {
-				return;
+				currentStep++;
+				goToStep(currentStep);
+			});
+			
+			// submit form
+			$('body').on('click', '.finish', function(event){
+				if (!validateForm($(this))) {
+					return;
+				}
+				
+				$this.closest('form').submit();
+			});
+		}
+
+		function goToStep(step)
+		{
+			$('.step').hide();
+			$('.step[data-step="'+step+'"]').show();
+		}
+
+		function validateForm($this)
+		{
+			var form = $this.closest('form');
+			form.validate();
+
+			if (!form.valid()) {
+				form.validate().element(form.find('input:visible,select:visible'));
+				return false;
 			}
 
-			currentStep++;
-			goToStep(currentStep);
-        });
-        
-		// submit form
-		$('body').on('click', '.finish', function(){
-			if (!$(this).valid()) {
-				return;
-			}
-
-			toggleLoader();
-			setTimeout(function() {
-				form.submit();
-			}, 100);
-		});
-	}
-
-	function goToStep(step)
-	{
-		$('.step').hide();
-		$('.step[data-step="'+step+'"]').show();
-	}
-
-    $(document).ready(function(){
-        formNavigation();
+			return true;
+		}
+		
+		formNavigation();
+		
+		jQuery('.phone').inputmask("(99)99999-9999");
+		jQuery('.date').inputmask("99/99/9999");
     });
 })(jQuery);
